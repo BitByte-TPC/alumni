3532826f80fc9ed368eea9eadf90b77d19c5aed9
arpantarkas <arpantarkas@ymail.com>
2019-01-27 20:10:42 +0530

    changes

--- AluminiConnect/applications/alumniprofile/views.py
+++ AluminiConnect/applications/alumniprofile/views.py
@@ -4,11 +4,6 @@ from .models import Profile
 # Create your views here.
 
 def index(request, username):
-    users = Profile.objects.get(user__username = username)
-    return render(request, "alumniprofile/profile.html", {"users" : users})
-    
-=======
     user = Profile.objects.get(user__username = username)
     print(vars(user))
     return render(request, "alumniprofile/profile.html", vars(user))
->>>>>>> 13d6c5c07de04ad55fa7eed61f262045e4469818
