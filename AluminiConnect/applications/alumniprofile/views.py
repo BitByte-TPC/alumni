c9b05d61c1f4a063637236daba10ffba2121da1e
janglee123 <merupatel123@gmail.com>
2019-01-28 00:06:58 +0530

    changes to print data of user in profile page

--- AluminiConnect/applications/alumniprofile/views.py
+++ AluminiConnect/applications/alumniprofile/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
+from .models import Profile
 # Create your views here.
 
 def index(request, username):
-        print(username)
-        return render(request, 'alumniprofile/profile.html', {'username' : username})    
+        profile = Profile.objects.get( roll_no = '2017180')
+        profile.username = username
+        print(vars(profile))
+        return render(request, 'alumniprofile/profile.html', vars(profile))    
