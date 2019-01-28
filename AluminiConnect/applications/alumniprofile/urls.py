8076f09da927a262687c3cef2cde7b577254b419
arpantarkas <arpantarkas@ymail.com>
2019-01-28 16:17:16 +0530

    Backend linked & major changes

--- AluminiConnect/applications/alumniprofile/urls.py
+++ AluminiConnect/applications/alumniprofile/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'profile'
 
 urlpatterns = [
-    url(r'(?P<username>[\w.@+-]+)/$', views.index, name='index'),
+    url(r'(?P<username>[\w.@+-]+)/$', views.index, name='profile'),
 ]
--- AluminiConnect/applications/events_news/views.py
+++ AluminiConnect/applications/events_news/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import Event
 # Create your views here.
-def index(request):
-    return render(request, "events_news/home.html")
+def events(request):
+    events_list = Event.objects.filter()
+    return render(request, "events_news/index.html", {'events' : events_list})
 
 def event(request, id):
     e = Event.objects.get(event_id = id)
