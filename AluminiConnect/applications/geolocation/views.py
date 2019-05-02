f162a32a7067ba5e5f46f171687e5dfbc6bc5f04
janglee123 <merupatel123@gmail.com>
2019-05-03 02:51:28 +0530

    geolocation fixed

--- AluminiConnect/applications/geolocation/views.py
+++ AluminiConnect/applications/geolocation/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.db.models import Count
+# import json as simplejson
+from django.core import serializers
+# from rest_framework import serializers
+from applications.alumniprofile.models import Profile
 
 # Create your views here.
 def index(request):
-    return render(request, "geolocation/index.html")
+    city = Profile.objects.only('city')
+    data = serializers.serialize('json', city,fields=('city'))
+    print(data)
+    return render(request, "geolocation/index.html",{'city':data})
