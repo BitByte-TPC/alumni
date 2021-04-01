9748f9c022b9cdaf8f3730dd036da743f03dc9f7
shivansh <shivansh.dhiman@bharatpe.com>
2021-04-01 14:22:24 +0530

    added url pattern for 6-digit rollno

--- AluminiConnect/applications/alumniprofile/urls.py
+++ AluminiConnect/applications/alumniprofile/urls.py
@@ -6,4 +6,5 @@ app_name = 'profile'
 
 urlpatterns = [
     url(r'^(?P<username>[0-9]{7})/$', views.profile, name='profile'),
+    url(r'^(?P<username>[0-9]{6})/$', views.profile, name='profile'),
 ]
