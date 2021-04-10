4fbd542421b5bf84bab9fbf464adafbe10e871f5
shivansh <shivanshdhiman@gmail.com>
2021-04-10 18:22:14 +0530

    reformat file

--- AluminiConnect/applications/job_posting/views.py
+++ AluminiConnect/applications/job_posting/views.py
@@ -12,6 +12,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def index(request):
     posts = Posting.objects.all().order_by('-posting_date')
     page = request.GET.get('page', 1)
+    alumni = []
     if posts:
         paginator = Paginator(posts, 5)
         try:
@@ -21,23 +22,23 @@ def index(request):
         except EmptyPage:
             ls2 = paginator.page(paginator.num_pages)
         if request.user.is_superuser:
-            ls=[]
+            ls = []
             for i in posts:
-                p = Profile.objects.get(name = i.person)
+                p = Profile.objects.get(name=i.person)
                 ls.append(p.roll_no)
-            ls1 = zip(ls2,ls)
-            return render(request, "job_posting/home.html", {'ls1':ls1,'ls2':ls2})
+            ls1 = zip(ls2, ls)
+            return render(request, "job_posting/home.html", {'ls1': ls1, 'ls2': ls2})
         else:
-            alumni = Profile.objects.get(roll_no = str(request.user))
-            ls=[]
+            alumni = Profile.objects.get(roll_no=str(request.user))
+            ls = []
             for i in posts:
-                p = Profile.objects.get(name = i.person)
+                p = Profile.objects.get(name=i.person)
                 ls.append(p.roll_no)
-            ls1 = zip(ls2,ls)
-            return render(request, "job_posting/home.html", {'ls1':ls1,'alumni':alumni,'ls2':ls2})
+            ls1 = zip(ls2, ls)
+            return render(request, "job_posting/home.html", {'ls1': ls1, 'alumni': alumni, 'ls2': ls2})
     else:
-        ls1=[]
-        return render(request, "job_posting/home.html", {'ls1':ls1,'alumni':alumni})
+        ls1 = []
+        return render(request, "job_posting/home.html", {'ls1': ls1, 'alumni': alumni})
 
 
 def filter(request):
@@ -45,19 +46,19 @@ def filter(request):
     page = request.GET.get('page', 1)
     type = request.POST.get('type')
     if position and not type:
-        posts = Posting.objects.filter(position = position).order_by('-posting_date')
+        posts = Posting.objects.filter(position=position).order_by('-posting_date')
         print(posts)
     elif type and not position:
-        posts = Posting.objects.filter(Q(type = type) | Q(type = 'Both')).order_by('-posting_date')
+        posts = Posting.objects.filter(Q(type=type) | Q(type='Both')).order_by('-posting_date')
         print(posts)
     else:
-        if type=='Both':
+        if type == 'Both':
             posts = Posting.objects.filter(position=position).order_by('-posting_date')
         else:
-            posts = Posting.objects.filter(Q(position = position) & Q(type = type)).order_by('-posting_date')
+            posts = Posting.objects.filter(Q(position=position) & Q(type=type)).order_by('-posting_date')
         print(posts)
     if posts:
-        paginator = Paginator(posts,5)
+        paginator = Paginator(posts, 5)
         try:
             ls2 = paginator.page(page)
         except PageNotAnInteger:
@@ -65,36 +66,39 @@ def filter(request):
         except EmptyPage:
             ls2 = paginator.page(paginator.num_pages)
         if request.user.is_superuser:
-            ls=[]
+            ls = []
             for i in posts:
-                p = Profile.objects.get(name = i.person)
+                p = Profile.objects.get(name=i.person)
                 ls.append(p.roll_no)
-            ls1 = zip(ls2,ls)
-            return render(request, "job_posting/filter.html", {'ls1':ls1,'ls2':ls2})
+            ls1 = zip(ls2, ls)
+            return render(request, "job_posting/filter.html", {'ls1': ls1, 'ls2': ls2})
         else:
-            ls=[]
+            ls = []
             for i in posts:
-                p = Profile.objects.get(name = i.person)
+                p = Profile.objects.get(name=i.person)
                 ls.append(p.roll_no)
-            ls1 = zip(ls2,ls)
-            alumni = Profile.objects.get(roll_no = str(request.user))
-            return render(request, "job_posting/filter.html",{'ls1':ls1,'alumni':alumni,'ls2':ls2})
+            ls1 = zip(ls2, ls)
+            alumni = Profile.objects.get(roll_no=str(request.user))
+            return render(request, "job_posting/filter.html", {'ls1': ls1, 'alumni': alumni, 'ls2': ls2})
     else:
-        ls1=[]
-        return render(request,"job_posting/filter.html",{'ls1':ls1})
+        ls1 = []
+        return render(request, "job_posting/filter.html", {'ls1': ls1})
+
 
 def new_post(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         job_t = request.POST.get('position')
         job_d = request.POST.get('duration')
         desc = request.POST.get('desc')
         link = request.POST.get('link')
         city = request.POST.get('city')
-        person = Profile.objects.get(roll_no = str(request.user))
-        insert = Posting.objects.create(position = job_t, type = job_d, desc = desc, link = link, posting_date = date.today(), location = city, person = person )
-    return redirect('jobs:index',permanent=True)
+        person = Profile.objects.get(roll_no=str(request.user))
+        insert = Posting.objects.create(position=job_t, type=job_d, desc=desc, link=link, posting_date=date.today(),
+                                        location=city, person=person)
+    return redirect('jobs:index', permanent=True)
 
-def del1(request,i_id=None):
+
+def del1(request, i_id=None):
     object = Posting.objects.get(id=i_id)
     object.delete()
-    return redirect('jobs:index',permanent=True)
+    return redirect('jobs:index', permanent=True)
