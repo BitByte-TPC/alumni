48d02fc8fc01f9d2a71983f4af2a0423fb320497
Tushar Maheshwari <tusharmaheshwaritm6@gmail.com>
2021-04-14 11:03:32 +0530

    Improved UI and filter

--- AluminiConnect/applications/job_posting/views.py
+++ AluminiConnect/applications/job_posting/views.py
@@ -13,7 +13,6 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def index(request):
     posts = Posting.objects.all().order_by('-posting_date')
     page = request.GET.get('page', 1)
-    alumni = []
     if posts:
         paginator = Paginator(posts, 5)
         try:
@@ -30,19 +29,19 @@ def index(request):
             ls1 = zip(ls2, ls)
             return render(request, "job_posting/home.html", {'ls1': ls1, 'ls2': ls2})
         else:
-            alumni = Profile.objects.get(roll_no=str(request.user))
             ls = []
             for i in posts:
                 p = Profile.objects.get(name=i.person)
                 ls.append(p.roll_no)
             ls1 = zip(ls2, ls)
-            return render(request, "job_posting/home.html", {'ls1': ls1, 'alumni': alumni, 'ls2': ls2})
+            return render(request, "job_posting/home.html", {'ls1': ls1, 'ls2': ls2})
     else:
         ls1 = []
-        return render(request, "job_posting/home.html", {'ls1': ls1, 'alumni': alumni})
+        return render(request, "job_posting/home.html", {'ls1': ls1})
 
 
 def filter(request):
+    viewname = "filter";
     position = request.POST.get('position')
     page = request.GET.get('page', 1)
     type = request.POST.get('type')
@@ -59,6 +58,7 @@ def filter(request):
             posts = Posting.objects.filter(Q(position=position) & Q(type=type)).order_by('-posting_date')
         print(posts)
     if posts:
+        messages.success(request, "Found " + str(posts.count()) + " posts matching your query!")
         paginator = Paginator(posts, 5)
         try:
             ls2 = paginator.page(page)
@@ -66,24 +66,17 @@ def filter(request):
             ls2 = paginator.page(1)
         except EmptyPage:
             ls2 = paginator.page(paginator.num_pages)
-        if request.user.is_superuser:
-            ls = []
-            for i in posts:
-                p = Profile.objects.get(name=i.person)
-                ls.append(p.roll_no)
+
+        ls = []
+        for i in posts:
+            p = Profile.objects.get(name=i.person)
+            ls.append(p.roll_no)
             ls1 = zip(ls2, ls)
-            return render(request, "job_posting/filter.html", {'ls1': ls1, 'ls2': ls2})
-        else:
-            ls = []
-            for i in posts:
-                p = Profile.objects.get(name=i.person)
-                ls.append(p.roll_no)
-            ls1 = zip(ls2, ls)
-            alumni = Profile.objects.get(roll_no=str(request.user))
-            return render(request, "job_posting/filter.html", {'ls1': ls1, 'alumni': alumni, 'ls2': ls2})
+            return render(request, "job_posting/home.html", {'ls1': ls1, 'ls2': ls2, 'viewname': viewname})
     else:
         ls1 = []
-        return render(request, "job_posting/filter.html", {'ls1': ls1})
+        messages.error(request, "No posts matching your current requirements.")
+        return render(request, "job_posting/home.html", {'ls1': ls1, 'viewname': viewname})
 
 def post(request):
     return render(request, "job_posting/post.html")
