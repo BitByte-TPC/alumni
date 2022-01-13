2364d220edd3287d568c8f2a9491812d07a1c74b
Priyansh Garg <priyanshgarg30@gmail.com>
2022-01-13 19:27:04 +0530

    Fix broken environ variables in production settings.

--- AluminiConnect/AluminiConnect/settings/production.py
+++ AluminiConnect/AluminiConnect/settings/production.py
@@ -1,10 +1,12 @@
+import ast
+
 from AluminiConnect.settings.common import *
 
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY_ALUMNI")
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['58.84.25.100', '103.59.142.75', 'https://www.iiitdmj.ac.in/sac.iiitdmj.ac.in/', 'sac.iiitdmj.ac.in']
+ALLOWED_HOSTS = ast.literal_eval(os.environ.get("ALLOWED_HOSTS"))
 
 # EMAIL SETTINGS
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
@@ -13,10 +15,10 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = os.environ.get("EMAIL_PORT", cast=int)
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
 
 DEFAULT_FROM_EMAIL = os.environ.get("SENDER_EMAIL")
-BCC_EMAILS = os.environ.get("BCC_EMAILS")
+BCC_EMAILS = ast.literal_eval(os.environ.get("BCC_EMAILS"))
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static/')
