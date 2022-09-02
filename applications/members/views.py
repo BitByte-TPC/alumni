b891a1a6c3ecbfb1deeca933309e30c6da4b22a8
Mihir <20bcs131@iiitdmj.ac.in>
2022-09-02 11:22:19 +0530

    required field removed

--- applications/members/views.py
+++ applications/members/views.py
@@ -1,3 +1,4 @@
+from importlib.metadata import requires
 import json
 
 from django.shortcuts import render, redirect
@@ -12,11 +13,15 @@ from applications.alumniprofile.models import Profile
 
 def index(request):
     counts = Profile.objects.filter(verify=True).values('batch').order_by('-batch').annotate(count=Count('batch'))
-    # print(len(counts))
     total = 0
     for batch, count in counts.values_list('batch', 'count'):
         total += count
-    return render(request, "members/index.html", {'data': counts.values_list('batch', 'count'), 'total': total})
+    data = counts.values_list('batch', 'count')
+    context = {
+               'data': data,
+               'total': total,
+               }
+    return render(request, "members/index.html",context)
 
 
 def batch(request, year):
@@ -48,35 +53,50 @@ def alumnibody(request):
 
 @login_required
 def search(request):
-    key = request.GET['search']
-    profiles = Profile.objects.filter(name__icontains=key) | Profile.objects.filter(
-        roll_no__icontains=key) | Profile.objects.filter(reg_no__icontains=key)
+    profiles = Profile.objects.all()
+    val=0
     if len(request.GET) > 1:
+
+        if request.GET['search'] != '':
+           val=val+1
+           key = request.GET['search']
+           profiles = Profile.objects.filter(name__icontains=key) | Profile.objects.filter(
+           roll_no__icontains=key) | Profile.objects.filter(reg_no__icontains=key)
+
         if request.GET['batch'] != '':
+            val=val+1
             batch = request.GET['batch']
-            print(batch)
             profiles = profiles.filter(batch=batch)
-            print(profiles)
+            
         if request.GET['city'] != '':
+            val=val+1
             city = request.GET['city']
             profiles = profiles.filter(city__icontains=city)
-        if request.GET['programme'] != 'Programme':
+
+        if request.GET['programme'] != '':
+            val=val+1
             programme = request.GET['programme']
             profiles = profiles.filter(programme__icontains=programme)
+
         if request.GET['branch'] != '':
+            val=val+1
             branch = request.GET['branch']
             profiles = profiles.filter(branch__icontains=branch)
-        if request.GET['org'] != '':
+
+        if request.GET['org'] != '':  
+            val=val+1
             org = request.GET['org']
             profiles1 = profiles.filter(current_organisation__icontains=org)
             profiles2 = profiles.filter(current_university__icontains=org)
             profiles = profiles1 | profiles2
+
     profiles = profiles.order_by('name')
     context = {'profiles': profiles,
-               'keyy': key,
+               'keyy': val,
                'zero': len(profiles),
                'request': request.GET
                }
+
     return render(request, "members/index.html", context)
 
 
@@ -87,7 +107,6 @@ def autoSearch(request):
             roll_no__icontains=key) | Profile.objects.filter(reg_no__icontains=key)
         data = []
         for r in search_qs:
-            print(r.name)
             data.append(r.name)
     else:
         data = 'fail'
@@ -96,13 +115,14 @@ def autoSearch(request):
 
 @login_required
 def mapSearch(request):
-    key = request.GET['search']
-    city = key.split(',', 1)[0]
-    profiles = Profile.objects.filter(city__icontains=city)
-    profiles = profiles.order_by('name')
-    context = {'profiles': profiles,
+    if request.GET['search']:
+       key = request.GET['search']
+       city = key.split(',', 1)[0]
+       profiles = Profile.objects.filter(city__icontains=city)
+       profiles = profiles.order_by('name')
+       context = {'profiles': profiles,
                'keyy': key,
                'zero': len(profiles),
                'map': True
-               }
+                }
     return render(request, "members/index.html", context)
