d885947f7e127a67816160be27fc18647549dab4
Priyansh Garg <priyanshgarg30@gmail.com>
2024-01-07 02:08:15 +0530

    Don't show any profile is search criteria is empty.

--- applications/members/views.py
+++ applications/members/views.py
@@ -58,48 +58,59 @@ def alumnibody(request):
 @login_required
 def search(request):
     profiles = Profile.objects.all()
+    profiles_filtered = False
+
     if len(request.POST) > 1:
         if request.POST['search'] != '':
-           key = request.POST['search']
-           profiles = profiles.filter(name__icontains=key) | profiles.filter(
-           roll_no__icontains=key) | profiles.filter(reg_no__icontains=key)
+            key = request.POST['search']
+            profiles = profiles.filter(name__icontains=key) | profiles.filter(
+            roll_no__icontains=key) | profiles.filter(reg_no__icontains=key)
+            profiles_filtered = True
 
         if request.POST['batch'] != '':
             batch = request.POST['batch']
             profiles = profiles.filter(batch=batch)
+            profiles_filtered = True
             
         if request.POST['city'] != '':
             city = request.POST['city']
             profiles = profiles.filter(city__icontains=city)
+            profiles_filtered = True
 
         if request.POST['programme'] != '':
             programme = request.POST['programme']
             profiles = profiles.filter(programme__icontains=programme)
+            profiles_filtered = True
 
         if request.POST['branch'] != '':
             branch = request.POST['branch']
             profiles = profiles.filter(branch__icontains=branch)
+            profiles_filtered = True
 
         if request.POST['org'] != '':  
             org = request.POST['org']
             profiles1 = profiles.filter(current_organisation__icontains=org)
             profiles2 = profiles.filter(current_university__icontains=org)
             profiles = profiles1 | profiles2
+            profiles_filtered = True
 
     profiles = profiles.order_by('roll_no')
 
-    context = {'profiles': profiles,
-               'keyy': 1,
-               'zero': len(profiles),
-               'request': request.POST
-               }
+    if not profiles_filtered:
+        # return empty queryset in case no filters are selected
+        profiles = Profile.objects.none()
+
+    context = {
+        'profiles': profiles,
+        'keyy': 1,
+        'zero': len(profiles),
+        'request': request.POST
+    }
     
     if len(profiles):
-        messages.success(request,"Total "+str(len(profiles))+" Alumni Found")
+        messages.success(request, f'Total {str(len(profiles))} Alumni Found')
     else:
-         messages.error(request, "No Result Found ")
-
-        
+        messages.error(request, "No Result Found")
 
     return render(request, "members/index.html", context)
 
