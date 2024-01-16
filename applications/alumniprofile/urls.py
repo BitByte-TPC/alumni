13bd389a59629271fab22a47859c8ceca3456678
Priyansh Garg <priyanshgarg30@gmail.com>
2024-01-16 22:18:23 +0530

    Fix regex in alumni profile url.

--- applications/alumniprofile/urls.py
+++ applications/alumniprofile/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'profile'
 
 urlpatterns = [
-    re_path(r'^(?P<username>[a-zA-z0-9]{5,9})/$', views.profile, name='profile'),
+    re_path(r'^(?P<username>[a-zA-Z0-9]{5,9})/$', views.profile, name='profile'),
     path('add_experience', views.add_experience, name='add_experience'),
     path('add_education', views.add_education, name='add_education'),
 ]
