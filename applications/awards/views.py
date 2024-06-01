905ea76f8fdc1f9b03e4c91ad0387d3a92ad842e
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-06-02 03:49:40 +0530

    Modified the views.py file to render the N no.of awards on the webpage

--- applications/awards/views.py
+++ applications/awards/views.py
@@ -4,7 +4,9 @@ from .models import Award
 
 # Create your views here.
 def index(request):
-    return render(request, "awards/index.html")
+    awards = Award.objects.all()
+    award_count = awards.count()
+    return render(request, "awards/index.html", {"awards": awards, "award_count": award_count})
 
 
 def award(request, id):
