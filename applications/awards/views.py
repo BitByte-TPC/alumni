0c9eb082d3f91891713a99b1428f7263151b8340
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-06-06 10:16:35 +0530

    Made the views.py file in  sync with the latest chnages made in the repo and resolved all merge conflicts

--- applications/awards/views.py
+++ applications/awards/views.py
@@ -4,6 +4,7 @@ from .models import Award
 # Create your views here.
 def index(request):
     awards = Award.objects.all()
+    
     award_count = awards.count()
     return render(request, "awards/index.html", {"awards": awards, "award_count": award_count})
 
