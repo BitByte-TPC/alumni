d56b89e65457bca0554ffda3d37e3a9685061a55
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-07-07 04:30:04 +0530

    Updated views to trim out text for better display and other modification

--- applications/awards/views.py
+++ applications/awards/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render ,redirect
+# applications/awards/views.py
+from django.shortcuts import render, redirect
 from .models import Award
 
-# Create your views here.
 def index(request):
     awards = Award.objects.all()
-    
+    for award in awards:
+        award.short_description = ' '.join(award.description.split()[:50]) 
+
     award_count = awards.count()
     return render(request, "awards/index.html", {"awards": awards, "award_count": award_count})
-
 
 def award(request, id):
     try:
         award = Award.objects.get(award_id=id)
         return render(request, "awards/award.html", {"award": award})
-    except:
+    except Award.DoesNotExist:
         return redirect('awards:index')
