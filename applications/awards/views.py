47b8aa7b93cfc8fdc456cbcf34a35dde3855304e
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-07-13 13:39:18 +0530

    updated views

--- applications/awards/views.py
+++ applications/awards/views.py
@@ -1,11 +1,11 @@
-# applications/awards/views.py
 from django.shortcuts import render, redirect
 from .models import Award
 
 def index(request):
     awards = Award.objects.all()
     for award in awards:
-        award.short_description = ' '.join(award.description.split()[:50]) 
+        words = award.description.split()
+        award.short_description = ' '.join(words[:50]) + ('...' if len(words) > 50 else '')
 
     award_count = awards.count()
     return render(request, "awards/index.html", {"awards": awards, "award_count": award_count})
