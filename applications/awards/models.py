4cdbbd437a03b7de72b88176a13aba2dbff1f217
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-07-14 00:27:05 +0530

    updated models

--- applications/awards/models.py
+++ applications/awards/models.py
@@ -2,14 +2,24 @@ from django.db import models
 from ckeditor_uploader.fields import RichTextUploadingField
 from ckeditor.fields import RichTextField
 from django.utils.html import strip_tags
+from django.core.validators import MaxLengthValidator
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
 
 class Award(models.Model):
     award_id = models.AutoField(primary_key=True)
     title = RichTextField()
     by = models.CharField(max_length=255, null=True)
-    received_by = models.CharField(max_length=255, null=True, blank=True)  
-    description = RichTextUploadingField()
-    image = models.ImageField(upload_to='awards/images/', null=True, blank=True)
+    received_by = models.CharField(max_length=255, null=True, blank=True)
+    description = RichTextUploadingField(validators=[MaxLengthValidator(2000)])
+    image = ProcessedImageField(
+        upload_to='awards/images/',
+        processors=[ResizeToFit(50, 50)],
+        format='JPEG',
+        options={'quality': 90},
+        null=True,
+        blank=True
+    )
     published_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
@@ -22,8 +32,3 @@ class Award(models.Model):
     @property
     def description_snippet(self):
         return ' '.join(strip_tags(self.description).split()[:50]) + ('...' if len(strip_tags(self.description).split()) > 50 else '')
-
-    def save(self, *args, **kwargs):
-        if len(strip_tags(self.description).split()) > 2000:
-            self.description = ' '.join(strip_tags(self.description).split()[:2000])
-        super().save(*args, **kwargs)
