e521e1f17a809b12f588cb17087baf9bfef7e7f7
RITANKAR SAHA <ritankar.saha786@gmail.com>
2024-07-17 18:05:31 +0530

    Fixes #168 Updated the entire Awards Section (#169)
    * updated model to add received_by and image fields
    
    * Updated views to trim out text for better display and other modification
    
    * Updated urls to take in double-digit ids
    
    * Updated the award.html page to display image and received_by + UI fixes
    
    * Updated the index.html page to display image and received_by + UI fixes
    
    * updated models
    
    * updated views
    
    * updated the index page
    
    * updated models
    
    * index file changes little
    
    * Resized the image to 300*300
    
    * Center aligned the image

--- applications/awards/models.py
+++ applications/awards/models.py
@@ -1,16 +1,25 @@
 from django.db import models
-import datetime
 from ckeditor_uploader.fields import RichTextUploadingField
 from ckeditor.fields import RichTextField
-
-
-# Create your models here.
+from django.utils.html import strip_tags
+from django.core.validators import MaxLengthValidator
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
 
 class Award(models.Model):
     award_id = models.AutoField(primary_key=True)
     title = RichTextField()
     by = models.CharField(max_length=255, null=True)
-    description = RichTextUploadingField()
+    received_by = models.CharField(max_length=255, null=True, blank=True)
+    description = RichTextUploadingField(validators=[MaxLengthValidator(2000)])
+    image = ProcessedImageField(
+        upload_to='awards/images/',
+        processors=[ResizeToFit(300, 300)],
+        format='JPEG',
+        options={'quality': 90},
+        null=True,
+        blank=True
+    )
     published_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
@@ -18,5 +27,8 @@ class Award(models.Model):
 
     @property
     def title_stripped(self):
-        from django.utils.html import strip_tags
         return strip_tags(self.title)
+
+    @property
+    def description_snippet(self):
+        return ' '.join(strip_tags(self.description).split()[:50]) + ('...' if len(strip_tags(self.description).split()) > 50 else '')
--- applications/awards/views.py
+++ applications/awards/views.py
@@ -1,17 +1,18 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render, redirect
 from .models import Award
 
-# Create your views here.
 def index(request):
     awards = Award.objects.all()
-    
+    for award in awards:
+        words = award.description.split()
+        award.short_description = ' '.join(words[:50]) + ('...' if len(words) > 50 else '')
+
     award_count = awards.count()
     return render(request, "awards/index.html", {"awards": awards, "award_count": award_count})
-
 
 def award(request, id):
     try:
         award = Award.objects.get(award_id=id)
         return render(request, "awards/award.html", {"award": award})
-    except:
+    except Award.DoesNotExist:
         return redirect('awards:index')
